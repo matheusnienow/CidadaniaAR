e636c125df19a396c9754f1f71e901d98abceede
Matheus Navarro Nienow <matheusnienow@gmail.com>
2019-11-23 01:50:57 -0300

    corrigido animation controller e animações

---
+++ Assets/AnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator _animator;
+    private NavMeshAgent _navMeshAgent;
+    private static readonly int IsMoving = Animator.StringToHash("IsMoving");
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        var isMoving = _navMeshAgent.velocity != Vector3.zero;
+        _animator.SetBool(IsMoving, isMoving);
+    }
+}
